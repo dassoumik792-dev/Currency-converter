e8c5deebc8c2e313db5d42d23f1de5146998f4af
Soumik Das <dassoumik792@gmail.com>
2025-12-31 21:10:13 +0530

    Updated

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ for (let select of dropdowns) {
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
     newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
-      newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "INR") {
-      newOption.selected = "selected";
-    }
+    fromCurr.value="USD";
+    toCurr.value="INR";
+    
     select.append(newOption);
   }
 
